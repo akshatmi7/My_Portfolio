7fd2b932e1f78afb6b9ded912764f6edcacc42f4
akshatmi7 <akshatmishra2222@gmail.com>
2024-12-21 21:30:22 +0530

    Update changes

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  function launchRocket() {
+    // Trigger the rocket launch animation
+    const rocket = document.querySelector('.rocket-icon');
+    rocket.classList.add('rocket-launch');
+    
+    // Fade out the home page
+    const homeSection = document.querySelector('body');
+    homeSection.classList.add('fade-out');
+    
+    // Redirect to games page after animation
+    setTimeout(function() {
+      window.location.href = 'games.html';
+    }, 2000); // Wait for the animation to complete before redirecting
+  }
+  
+  
